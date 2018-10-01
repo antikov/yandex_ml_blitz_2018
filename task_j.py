8832cf1a95e7f0fb86c1f4c372941fcd82b12af7
antikov <crazytraffic@gmail.com>
2018-10-01 20:24:03 +0500

    better solution

--- task_j.py
+++ task_j.py
@@ -1,42 +1,16 @@
 import numpy as np
 
-class LR:
+class LinearSeparateBoundary:
     def __init__(self, m):
         self.w = np.random.rand(m, 1)
     
     def fit(self, x, y):
-        m, d = x.shape
-        t = 1
-        l = 1
-        for i in range(10):
-            #print(self.w)
-            for i in range(m):
-                if y[i] * (x[i] @ self.w)[0] < 1:
-                    self.w = (1 - 1/t) * self.w + 1 / (l * t) * y[i]*x[i].reshape(-1,1)
-                else:
-                    self.w *= (1-1/t)
-                t += 1
-            #c1=1-(y*(x @ self.w))
-            #c1[c1<0] = 0
-            #cost = self.w.T @ self.w + l * sum(c1)
-            #print(cost)
-        
-# t = 1;
-# for (i=1:iterations)      % iterations over the full data
-#     for (tau=1:m)      % pick a single data point
-#         if (y(tau)*x(tau,:)*w < 1)  
-#             w = (1-1/t)*w + 1/(lambda*t)*y(tau)*x(tau,:)';
-#         else
-#             w = (1-1/t)*w;
-#         end
-#         t=t+1;         % increment counter
-#     end
-#     c1=1-(y.*(x*w));
-#     c1(c1<0)=0;  
-#     cost = w'*w+ lambda*(sum(c1));
-#     fprintf('Cost:%f\n',cost);
-# end
-    
+        for i in range(len(x)):
+            if np.sign(self.w.T @ x[i]) != y[i]:
+                self.w += x[i] * y[i]
+
+    def get_weights(self):
+        return self.w.reshape(-1)
 
 n, m = [int(x) for x in input().split()]
 x = []
@@ -49,6 +23,6 @@ for _ in range(n):
 x = np.array(x)
 y = np.array(y)
 
-lr = LR(m)
+lr = LinearSeparateBoundary(m)
 lr.fit(x, y)
-print(*lr.w.reshape(-1))
+print(*lr.get_weights())
